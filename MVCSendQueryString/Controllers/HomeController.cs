5bb9af58867bfe6ac1382342e77f6620d0c76462
rafaelestevao <rafael-estevao@hotmail.com>
2018-04-01 15:30:18 -0300

    commit inicial

---
+++ MVCSendQueryString/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCSendQueryString.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(int id, int parcela)
+        {
+            ViewBag.texto = $"Retorno da execução - ID: {id} e Parcela: {parcela}";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
